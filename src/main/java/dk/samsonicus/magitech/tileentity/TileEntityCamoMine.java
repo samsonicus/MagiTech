a5c0e8cd6664d8f94b4c41f724a38bce50a4b8f8
Samsonicus <thor_v_terp@hotmail.com>
2016-10-30 01:59:57 +0200

    contenued work with tileEntities

--- src/main/java/dk/samsonicus/magitech/tileentity/TileEntityCamoMine.java
+++ src/main/java/dk/samsonicus/magitech/tileentity/TileEntityCamoMine.java
@@ -2,10 +2,15 @@ package dk.samsonicus.magitech.tileentity;
 
 
 import dk.samsonicus.magitech.utility.LogHelper;
-import dk.samsonicus.magitech.utility.NBTHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.List;
 
 /**
  * Created by 5k on 26-10-2016.
@@ -16,26 +21,29 @@ public class TileEntityCamoMine extends TileEntity implements ITickable {
 
     @Override
     public void update() {
-        timer--;
-        LogHelper.info(timer);
-        if(timer == 0 && !worldObj.isRemote){
-            worldObj.createExplosion(null,pos.getX() + 0.5,pos.getY()+0.5,pos.getZ()+0.5,3f,true);
+        if (timer > 0) timer--;
+        //LogHelper.info(timer);
+        if(timer <= 0 && !worldObj.isRemote){
+            List<Entity> entities = worldObj.getEntitiesWithinAABB(Entity.class,new AxisAlignedBB(getPos()).expandXyz(1));
+            if (entities.size()>0) {
+                worldObj.createExplosion(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 3f, true);
+            }
         }
     }
+
 
     @Override
     public void readFromNBT(NBTTagCompound compound){
         super.readFromNBT(compound);
         timer = compound.getInteger("timer");
 
+
     }
 
     @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound compound){
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
         compound.setInteger("timer",timer);
         return compound;
     }
-
-
 }
